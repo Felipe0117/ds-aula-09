24cccf073d35d151ce8c5b07d9507d26eeff70e5
Felipe0117 <felipe.pastorello2006@gmail.com>
2024-03-04 17:30:16 -0300

    js do pokemon e login 100%

--- js/login.js
+++ js/login.js
@@ -4,20 +4,21 @@ const taskSenha = document.getElementById("taskSenha");
 const entrarTask = document.getElementById("entrarTask");
 
 entrarTask.addEventListener("click", function() {
-    window.location.href = "/pages/pagina1.html";
+    
+    
     
     if(taskUser.value.trim() == "" || taskSenha.value.trim() == "" ){
         alert("Escreva algo");
 
-    }else{
-        if(taskUser.value.trim() == "admin" || taskSenha.value.trim() == "admin" ){
+    }else if(taskUser.value.trim() == "admin" && taskSenha.value.trim() == "admin" ){
             alert("Proxima página");
+            window.location.href = "pages/pagina1.html";
         }
         else{
             alert("Usuário ou senha inválidos");
 
         }
 
-    }
-});   
+    
 });
+});   
